dc6aafc85834e037d3ccb8b89be4fa45c61e6e3b
Tylinn <tgonzalezb@est.utn.ac.cr>
2023-11-23 00:11:06 -0600

    Problema 1: Listo. Form -> Prueba1 de practica

---
+++ Capas/GUI/Prueba1.cs
@@ -0,0 +1,39 @@
+﻿using appNotas.Capas.Entidades;
+using appNotas.Capas.Logica;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace appNotas.Capas.GUI
+{
+    public partial class Prueba1 : Form
+    {
+        public Prueba1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            DALNotas dalNotas = new DALNotas();
+            ProblemaUno oProblema = new ProblemaUno();
+            oProblema.listNotas = dalNotas.GetAllNotas();
+
+            //Sexto
+            rtxtProm6.Text = Convert.ToString(oProblema.GetPromedio6());
+            rtxtProm5.Text = Convert.ToString(oProblema.GetPromedio5());
+            rtxtDesvEst6.Text = Convert.ToString(oProblema.GetDesvEstandar6());
+            rtxtZ6.Text = Convert.ToString(oProblema.GetTotalZ6());
+            rtxtDifZ6.Text = Convert.ToString(oProblema.GetRestaZ6());
+
+            //Quinto
+            rtxtZ5.Text = Convert.ToString(oProblema.GetTotalZ5());
+            rtxtDifZ5.Text = Convert.ToString(oProblema.GetRestaZ5());
+
+            //Interpretación
+            rtxtInt.Text = oProblema.ToString();
+
+        }
+    }
+}
--- Capas/Logica/ProblemaUno.cs
+++ Capas/Logica/ProblemaUno.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using appNotas.Capas.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,126 @@ namespace appNotas.Capas.Logica
 {
     public class ProblemaUno
     {
+        public List<Notas> listNotas;
+
+        //Cálculo de los promedios
+        public double GetPromedio6()
+        {
+            double promSum = 0;
+            int cantidad = 0;
+
+            foreach (var promedio in listNotas)
+            {
+                promSum += promedio.Promedio6;
+                cantidad++;
+            }
+            return Math.Round(promSum / cantidad, 4);
+        }
+
+        public double GetPromedio5()
+        {
+            double promSum = 0;
+            int cantidad = 0;
+
+            foreach (var promedio in listNotas)
+            {
+                promSum += promedio.Promedio5;
+                cantidad++;
+            }
+            return Math.Round(promSum / cantidad, 4);
+        }
+
+        //Desviación Estándar 6 año
+
+        public double GetDesvEstandar6()
+        {
+            double xi = 0;
+            int cantidad = 0;
+
+            foreach (var promedio in listNotas)
+            {
+                xi += Math.Pow(promedio.Promedio6 - GetPromedio6(), 2);
+                cantidad++;
+            }
+
+            return Math.Round(Math.Sqrt(xi / (cantidad - 1)), 4);
+        }
+
+        //Z de 6año
+        public double GetZLi6()
+        {
+            double cantidad = 0, z = 2.33;
+
+            foreach (var promedio in listNotas)
+            {
+                cantidad++;
+            }
+
+            return Math.Round(GetPromedio6() - z * (GetDesvEstandar6() / Math.Sqrt(cantidad)), 4);
+        }
+
+        public double GetZLs6()
+        {
+            double cantidad = 0, z = 2.33;
+
+            foreach (var promedio in listNotas)
+            {
+                cantidad++;
+            }
+
+            return Math.Round(GetPromedio6() + z * (GetDesvEstandar6() / Math.Sqrt(cantidad)), 4);
+        }
+
+        public string GetTotalZ6()
+        {
+            return GetZLi6() + " ≤ µ ≤ " + GetZLs6();
+        }
+
+        public double GetRestaZ6()
+        {
+            return Math.Round(GetZLs6() - GetZLi6(), 4);
+        }
+
+        //Z de 5 año
+        public double GetZLi5()
+        {
+            double cantidad = 0, z = 2.33;
+
+            foreach (var promedio in listNotas)
+            {
+                cantidad++;
+            }
+
+            return Math.Round(GetPromedio5() - z * (GetDesvEstandar6() / Math.Sqrt(cantidad)), 4);
+        }
+
+        public double GetZLs5()
+        {
+            double cantidad = 0, z = 2.33;
+
+            foreach (var promedio in listNotas)
+            {
+                cantidad++;
+            }
+
+            return Math.Round(GetPromedio5() + z * (GetDesvEstandar6() / Math.Sqrt(cantidad)), 4);
+        }
+
+        public string GetTotalZ5()
+        {
+            return GetZLi5() + " ≤ µ ≤ " + GetZLs5();
+        }
+
+        public double GetRestaZ5()
+        {
+            return Math.Round(GetZLs5() - GetZLi5(), 4);
+        }
+
+        //Respuesta 
+        public override string ToString()
+        {
+            return "Gracias a un nivel de confianza del 98% se determina que los niveles " +
+                "de quinto y sexto año poseen porcentajes iguales en el total promedios obtenidos a lo largo del año"; 
+        }
     }
 }
